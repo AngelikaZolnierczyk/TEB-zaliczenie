974655f00ff6c2eca2cab80acb17db840703cec3
Angelika <Angelika.zolnierczyk@onet.pl>
2024-01-13 16:59:41 +0100

    Added project

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.tictactoe;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button[][] buttons = new Button[3][3];
+    private boolean playerX = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button" + (i * 3 + j + 1);
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(new View.OnClickListener() {
+
+
+                    @Override
+                    public void onClick(View view) {
+                        onGridButtonClick(view);
+                    }
+                });
+            }
+        }
+    }
+
+    public void onGridButtonClick(View view) {
+        Button button = (Button) view;
+        if (button.getText().toString().equals("")) {
+            if (playerX) {
+                button.setText("X");
+            } else {
+                button.setText("O");
+            }
+
+            if (checkForWin()) {
+                String winner = playerX ? "Player X" : "Player O";
+                Toast.makeText(this, winner + " wins!", Toast.LENGTH_SHORT).show();
+                resetGame();
+            } else if (checkForDraw()) {
+                Toast.makeText(this, "It's a draw!", Toast.LENGTH_SHORT).show();
+                resetGame();
+            } else {
+                playerX = !playerX;
+            }
+        } else {
+            Toast.makeText(this, "Choose an empty cell", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals("")) {
+                return true;
+            }
+            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals("")) {
+            return true;
+        }
+
+        if (field[0][2].equals(field[1][1]) && field[0][2].equals(field[2][0]) && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean checkForDraw() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (buttons[i][j].getText().toString().equals("")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void resetGame() {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        playerX = true;
+    }
+}
